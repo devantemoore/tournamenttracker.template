18e10e6dcbba1d3da4385163df088a000c6e8651
DeVante Moore <djm5696@gmail.com>
2021-07-03 21:39:21 -0500

    begin clean architecture attempt

---
+++ Domain/Entities/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tournamenttracker.template.Domain.Entities
+{
+    public class Player : User
+    {
+        public Guid PlayerId { get; set; }
+        public TeamRole teamRole { get; set; }
+    }
+
+    public enum TeamRole
+    {
+        player = 1,
+        lead = 2,
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tournamenttracker.template.Domain.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+    }
+}
