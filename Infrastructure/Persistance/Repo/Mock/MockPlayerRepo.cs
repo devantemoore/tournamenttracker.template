b7e6fc9d842d725e8573b8bf1fddc4503c246876
DeVante Moore <djm5696@gmail.com>
2021-07-14 11:53:01 -0500

    Added test cases for player CRUD

--- Infrastructure/Persistance/Repo/Mock/MockPlayerRepo.cs
+++ Infrastructure/Persistance/Repo/Mock/MockPlayerRepo.cs
@@ -12,12 +12,24 @@ namespace tournamenttracker.template.Infrastructure.Persistance.Mock
 
         public Player Create(Player player)
         {
-            throw new NotImplementedException();
+            var createdPlayer = new Player
+            {
+                Username = player.Username,
+                FirstName = player.FirstName,
+                LastName = player.LastName,
+            };
+            MockRepo.Players.Add(createdPlayer);
+            return createdPlayer;
         }
 
         public Player GetPlayer(Guid id)
         {
             return MockRepo.Players.First(p => p.PlayerId == id);
+        }
+
+        public ICollection<Player> GetPlayers()
+        {
+            return MockRepo.Players.ToList();
         }
     }
 }
