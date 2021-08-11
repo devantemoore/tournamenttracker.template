09b87258576991ca292fd9974c62fd87cfff5bce
DeVante Moore <djm5696@gmail.com>
2021-08-11 10:03:40 -0500

    developing requester tool

--- Core/Application/Interface/IServices/IPlayerService.cs
+++ Core/Application/Interface/IServices/IPlayerService.cs
@@ -7,7 +7,7 @@ using tournamenttracker.template.Domain.Entities;
 
 namespace tournamenttracker.template.Core.Application.Interface.IServices
 {
-    public interface IPlayerService
+    public interface IPlayerService : IRequesterService
     {
         public void ApproveRequest(AppRequest<Player, Team> request);
     }
